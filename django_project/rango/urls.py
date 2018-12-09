af1d14cd5ed655507fd82f243b58e6f3708a4a8a
yranda <gustavomiranda215@gmail.com>
2018-12-09 11:26:48 -0200

    Organizando as funcionalidades que faltam terminar

--- django_project/rango/urls.py
+++ django_project/rango/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
-from .views import Add_Category#, Add_Page
+from .views import Add_Category
+#, Add_Page
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -11,17 +12,17 @@ urlpatterns = [
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
 
-    #url(r'^like/$', views.like_category, name='like_category'),
+    url(r'^add_category/', Add_Category.as_view(), name='add_category'),
     url(r'^suggest/$', views.suggest_category, name='suggest_category'),
 
-    # url(r'^goto/$', views.track_url, name='goto'),
-    #url(r'^goto/(?P<page_id>[0-9]+)/$', views.track_url, name='goto'),
 
-    url(r'^add_category/', Add_Category.as_view(), name='add_category'),
+    ### Estão dando pau no sistema, retirar do base.html "add_category"
+
+    # url(r'^like/$', views.like_category, name='like_category'),
+    # url(r'^goto/$', views.track_url, name='goto'),
+    # url(r'^goto/(?P<page_id>[0-9]+)/$', views.track_url, name='goto'),
     # url(r'^add_category/', views.add_category, name='add_category'),
-    #url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
-    #    views.show_category, name='show_category'),
-    #url(r'^(?P<category_name_slug>[\w\-]+)/add_page/$',
-    #    Add_Page.as_view(), name='add_page'),
+    # url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+    # url(r'^(?P<category_name_slug>[\w\-]+)/add_page/$', Add_Page.as_view(), name='add_page'),
 
 ]
